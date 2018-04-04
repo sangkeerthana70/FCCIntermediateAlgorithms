3e105f9657cd09eebe48fcfa167cb13c4ffb7a5f
Anuradha <sangkeerthana@gmail.com>
2018-04-04 17:53:50 +0000

    Added convert HTML entity algorithm

---
+++ convertHTMLEntities.js
@@ -0,0 +1,35 @@
+function convertHTML(str) {
+  var newStr = str.split("");
+  console.log(newStr);
+  var reStr;
+  var i;
+  var newArray = [];
+	for (i = 0; i <newStr.length; i++) {
+	  var strElem = newStr[i];
+	  console.log(strElem);
+	  var matchCode = str.charCodeAt(i);
+	  console.log(matchCode);
+	  switch (strElem) {
+		case '&' :
+			strElem = '&amp;';
+			break;
+		case '<' :  
+			strElem = '&lt;';
+			break;
+		case '>' :
+			strElem = '&gt;';
+			break;
+		case '"':
+			strElem = '&quot;';
+			break;
+		case "'":
+			strElem = '&apos;';
+			break;
+	  }	
+		newArray.push(strElem);
+	    //console.log(newArray);
+    }
+	reStr = newArray.join('');
+	console.log(reStr);
+	return reStr; 
+}	
