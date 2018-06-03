bad2e7ae6be957ebedaf2b35042fc87399bc49d2
Anuradha <sangkeerthana@gmail.com>
2018-06-03 21:56:52 +0000

    modified findersKeepers.js

--- findersKeepers.js
+++ findersKeepers.js
@@ -11,6 +11,6 @@ function findElement(arr, func) {
 	
 }
 
-var result = findElement([1, 3, 5, 9], function(num){ return num % 2 === 0; });
+var result = findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
 console.log(result);
 
